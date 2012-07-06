120e2d3763debec1b9109ef3ac74af43b000ed1a
zkam <b1077006@levonline.com>
2012-07-06 14:15:09 +0200

    Little change

--- git_test1/Program.cs
+++ git_test1/Program.cs
@@ -9,7 +9,7 @@ namespace git_test1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("First test. ZKAM");
+            Console.WriteLine("First test. ZKAM. Hej");
         }
     }
 }
