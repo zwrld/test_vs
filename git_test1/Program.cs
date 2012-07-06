63ecf2e0dde789ccb6efa834bb875dfdddedf7df
zkam <b1077006@levonline.com>
2012-07-06 14:13:10 +0200

    This is my first test

---
+++ git_test1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace git_test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First test. ZKAM");
+        }
+    }
+}
